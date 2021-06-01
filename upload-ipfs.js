74f139b4bcb395606abf963db2220edf6f272fc1
Josedpv <mobilefl44@gmail.com>
2021-06-01 17:18:56 -0400

    reads PDFs and save into the ipfs infura network

--- upload-ipfs.js
+++ upload-ipfs.js
@@ -2,19 +2,29 @@ const fs = require('fs');
 //const IPFS = require('ipfs');
 const pdfparse = require('pdf-parse');
 const ipfsClient = require('ipfs-http-client');
+
 async function uploadIPFS(PATH){
-   await pdfparse(PATH).then(function (data) {
+   //readding PDF
+    await pdfparse(PATH).then(function (data) {
         console.log(data.text);
     })
+    var result = [];
+  
     try{
-        var node = await ipfsClient(PATH);
-        for await (const file of await node.add({
-            path: PATH,
+        var node = await ipfsClient({host:'ipfs.infura.io', port: '5001',protocol:'https'});//setting where to put it
+        for await (var file of await node.add({
+        
             content: fs.readFileSync(PATH)
         })){
-            return file.cid.toString();
+            result.push(file);
         }
-      
+        
+        
+        console.log(result[0].hash);
+
+        //example to download ==== https://ipfs.io/ipfs/+ "result[0].hash"
+      //https://ipfs.io/ipfs/QmXqNyw6pvfAWVKbpdYM6vET1RWX7Se86DBWn2nhG1Z4T5
+     
     }catch(err){
         console.log(err);
         return false;
